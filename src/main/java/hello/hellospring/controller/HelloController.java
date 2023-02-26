63afba6d7dbee7849d445c96c8bf758333264a19
Jess <nakedthunder00@gmail.com>
2023-02-27 05:57:29 +0900

    add controller and Thy leaf

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,24 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!!!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    //required 기본이 true
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+}
