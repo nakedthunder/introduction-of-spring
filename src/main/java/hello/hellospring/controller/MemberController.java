bf3ae993e56696067f54de0e3352932230d0a1a6
Jess <nakedthunder00@gmail.com>
2023-03-14 05:19:07 +0900

    add GetMapping, PostMapping for memberForm

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -16,6 +18,17 @@ public class MemberController {
 
     @GetMapping("/members/new")
     public String createForm() {
-       return "members/createMemberForm";
+       return "members/createMemberForm"; //회원등록폼
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        //System.out.println("member는 " + member.getName());
+        memberService.join(member);
+        //회원가입이 끝나면 home화면으로 보내버리는 것
+        return "redirect:/";
     }
 }
